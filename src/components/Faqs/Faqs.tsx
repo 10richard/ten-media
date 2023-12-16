d41e8f32ab9455c84c02d717d4a3345c682f5e90
Richard Ten <rten04@icloud.com>
2023-12-16 02:05:18 -0500

    add Faqs component and FaqsList

---
+++ src/components/Faqs/Faqs.tsx
@@ -0,0 +1,12 @@
+import SectionHeading from "../SectionHeading";
+import { FaqsList } from "./FaqsList";
+
+const Faqs = () => {
+  return (
+    <section id="faqs" className="flex justify-center py-[120px] bg-[#F8F9FC]">
+      <SectionHeading heading={"FAQs"} />
+    </section>
+  );
+};
+
+export default Faqs;
---
+++ src/components/Faqs/FaqsList.tsx
@@ -0,0 +1,43 @@
+interface FaqsListProps {
+  question: string;
+  answer: string;
+}
+
+export const FaqsList: FaqsListProps[] = [
+  {
+    question: "What is your pricing model?",
+    answer:
+      "We require a 50% upfront payment, with the remaining balance due upon project completion. This ensures a balanced and fair approach to our billing process.",
+  },
+  {
+    question: "How long does it take to complete a website?",
+    answer:
+      "Typically, our projects are completed within 1-2 weeks, with timelines varying depending on the project's scope and complexity.",
+  },
+  {
+    question: "How do I track my order?",
+    answer:
+      "We keep you in the loop at every stage of the process. Upon creating a mockup of your website, we share it with you. Once approved, we proceed with the development phase.",
+  },
+  {
+    question:
+      "I don’t have logo and branding for my company yet, can I still submit a project?",
+    answer:
+      "Absolutely! No logo, no problem. We'll craft a premium logo for your new site. Need social media setup? We've got you covered.",
+  },
+  {
+    question: "How is the cost determined for your services?",
+    answer:
+      "We calculate costs based on the specific needs of your website, including the number of pages, logo design, and social media integration.",
+  },
+  {
+    question: "What if I want to make changes to the website after it's live?",
+    answer:
+      "With our monthly maintenance service, you have the flexibility to request website changes at any time, ensuring your online presence stays dynamic and up-to-date.",
+  },
+  {
+    question: "Can you redesign an existing website?",
+    answer:
+      "Certainly! Many websites lag behind, both in style and speed, warranting an overdue upgrade.",
+  },
+];
