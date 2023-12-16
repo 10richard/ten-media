914ca52a8ba24133960c59d9bef7f278910a0133
Richard Ten <rten04@icloud.com>
2023-12-16 00:49:43 -0500

    add hover selector

--- src/components/Services/ServicesContainer.tsx
+++ src/components/Services/ServicesContainer.tsx
@@ -12,7 +12,11 @@ const ServicesContainer = ({
   altText,
 }: ServicesContainerProps) => {
   return (
-    <div className="flex flex-col flex-wrap gap-5 w-1/3 min-w-[300px] bg-[#F6F6F6] px-9 pt-14 pb-32 rounded-2xl relative hover:-translate-y-2 hover:scale-105 hover:shadow-xl duration-500">
+    <div
+      className="flex flex-col flex-wrap gap-5 w-1/3 min-w-[300px] bg-[#F6F6F6] px-9 pt-14 pb-32 
+    rounded-2xl relative hover:-translate-y-2 hover:scale-105 hover:shadow-xl border-2 border-transparent 
+    hover:border-primary group duration-500"
+    >
       <img src={img} alt={altText} className="w-[60px]" />
       <h2 className="text-2xl">{service}</h2>
       <p className="text-gray-400">{text}</p>
