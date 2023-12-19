aea70d3aa40212f8493184abbe9a16819e03abc8
Richard Ten <rten04@icloud.com>
2023-12-18 20:17:02 -0500

    change h1 to h2

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -6,7 +6,7 @@ interface SectionHeadingProps {
 const SectionHeading = ({ heading, subheading }: SectionHeadingProps) => {
   return (
     <div className="flex flex-col items-center gap-3 mb-[50px]">
-      <h1 className="text-6xl font-bold">{heading}</h1>
+      <h2 className="text-6xl font-bold">{heading}</h2>
       <p className="text-gray-400">{subheading}</p>
     </div>
   );
