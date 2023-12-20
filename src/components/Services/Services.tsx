bf47160285899246db5d3bd24bd0f1ac37c26798
Richard Ten <rten04@icloud.com>
2023-12-20 10:18:11 -0500

    modify services components and prop types

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -9,11 +9,16 @@ const Services = () => {
   return (
     <section id="services" className="flex flex-col items-center py-[120px]">
       <SectionHeading heading={"Our Services"} subheading={"What we do."} />
-      <div className="flex justify-center gap-8 max-w-[1280px]">
+      <div className="flex justify-center flex-wrap gap-8 max-w-[1280px] px-10">
         <ServicesContainer
           service={"Design"}
           text={
-            "We prioritize both simplicity and functionality to guarantee a website design that not only captivates but converts."
+            <p>
+              We prioritize both{" "}
+              <strong className="text-blue-400">simplicity</strong> and{" "}
+              <strong className="text-blue-400">functionality</strong> to
+              guarantee a website design that not only captivates but converts.
+            </p>
           }
           img={designIcon.src}
           altText={"TenMedia Website Design icon"}
@@ -21,7 +26,12 @@ const Services = () => {
         <ServicesContainer
           service={"Development"}
           text={
-            "Each website is crafted with responsiveness and SEO in mind, ensuring your content thrives seamlessly online."
+            <p>
+              Each website is crafted with{" "}
+              <strong className="text-blue-400">responsiveness</strong> and{" "}
+              <strong className="text-blue-400">SEO</strong> in mind, ensuring
+              your content thrives seamlessly online.
+            </p>
           }
           img={developmentIcon.src}
           altText={"TenMedia Website Development icon"}
@@ -29,7 +39,12 @@ const Services = () => {
         <ServicesContainer
           service={"Maintenance"}
           text={
-            "Swift content updates, seamless hosting, and proactive security measures keep your website dynamic, optimized, and secure."
+            <p>
+              <strong className="text-blue-400">Swift</strong> content updates,{" "}
+              <strong className="text-blue-400">seamless</strong> hosting, and{" "}
+              <strong className="text-blue-400">proactive</strong> security
+              measures keep your website dynamic, optimized, and secure.
+            </p>
           }
           img={maintenanceIcon.src}
           altText={"TenMedia Website Maintenance icon"}
--- src/components/Services/ServicesContainer.tsx
+++ src/components/Services/ServicesContainer.tsx
@@ -1,6 +1,8 @@
+import React from "react";
+
 interface ServicesContainerProps {
   service: string;
-  text: string;
+  text: React.ReactNode;
   img: string;
   altText: string;
 }
@@ -13,13 +15,13 @@ const ServicesContainer = ({
 }: ServicesContainerProps) => {
   return (
     <div
-      className="flex flex-col flex-wrap gap-5 w-1/3 min-w-[300px] bg-[#F6F6F6] px-8 pt-14 pb-32 
+      className="flex flex-col flex-wrap gap-5 w-[30%] min-w-[325px] bg-[#F6F6F6] px-8 pt-14 pb-32 
     rounded-2xl relative hover:-translate-y-2 hover:scale-105 hover:shadow-xl border-2 border-transparent 
     hover:border-primary group duration-500"
     >
       <img src={img} alt={altText} className="w-[60px]" />
       <h2 className="text-2xl">{service}</h2>
-      <p className="text-gray-400">{text}</p>
+      <div className="text-gray-400">{text}</div>
     </div>
   );
 };
