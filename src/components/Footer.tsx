4bd23b618217749a4625deb88e8d3eff1cc7f456
Richard Ten <rten04@icloud.com>
2023-12-20 11:33:22 -0500

    modify width of footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,11 @@ import tenmediaLogo from "../assets/tenmedia-logo.png";
 const Footer = () => {
   return (
     <footer className="flex justify-between items-center py-8 px-12 bg-[#00010C] text-white">
-      <img src={tenmediaLogo.src} alt="TenMedia Logo" className="w-[200px]" />
+      <img
+        src={tenmediaLogo.src}
+        alt="TenMedia Logo"
+        className="max-w-[200px] w-[75%]"
+      />
       <p className="text-[#C6C9D8]">© 2023 TenMedia. All rights reserved.</p>
     </footer>
   );
